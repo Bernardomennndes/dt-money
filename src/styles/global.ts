0ce207824dd61f4e8d0509ce3394c0bfaaed2e64
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-02-11 12:20:09 -0300

    global style applied

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --blue: #5429CC;
+        --green: #33CC95; 
+        --red: #E62E4D;
+        --mainShape: #FFFFFF;
+        --titles: #363F5F;
+        --text: #969CB3;
+        --background: #F0F2F5;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1080px) { font-size: 93.75% }
+        @media (max-width: 720px) { font-size: 87.5% }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button { 
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+
+`
