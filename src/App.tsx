2edb04b263e7fb45ed718840dfc5b63ecf787936
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-02-22 11:45:15 -0300

    modal componentization

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
 import { useState } from 'react';
-import  Modal  from 'react-modal';
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
 export function App() {
 
-  
+
   const [isNewTranactionModalOpen, setIsNewTranactionModalOpen] = useState(false);
-    
+
   const handleOpenNewTrasactionModal = () => {
-      setIsNewTranactionModalOpen(true);
+    setIsNewTranactionModalOpen(true);
   }
 
   const handleCloseNewTransactionModal = () => {
-      setIsNewTranactionModalOpen(false);
+    setIsNewTranactionModalOpen(false);
   }
 
   return (
     <>
-      <Header onOpenNewTrasactionModal={handleOpenNewTrasactionModal}/>
-
+      <Header onOpenNewTrasactionModal={handleOpenNewTrasactionModal} />
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTranactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Registrer transaction</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
